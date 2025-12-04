3e16f38ee5bc1e3df94e54be06ba2fc7189b0c41
Kincent Lan <klanlincoln@gmail.com>
2025-12-04 00:38:31 -0600

    Day 1 and Day 2

---
+++ src/day1.kt
@@ -0,0 +1,57 @@
+import java.io.File
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+import kotlin.math.abs
+
+fun rotate(dial: Int, magnitude: Int): Int {
+     val diff = dial + magnitude
+     val nextDial = diff % 100
+     return if(nextDial < 0) { 100 + nextDial } else { nextDial }
+}
+
+fun calculateMagnitude(rotation: String): Int {
+    val sign = if (rotation[0] == 'L') { -1 } else { 1 }
+    val number = rotation.slice(1 until rotation.length).toInt()
+    return sign * number
+}
+
+fun getPassword(rotationList: File): Int {
+    var zeroCounter = 0
+    var dial = 50
+    rotationList.forEachLine {
+        val magnitude = calculateMagnitude(it)
+        dial = rotate(dial, magnitude)
+        if (dial == 0) zeroCounter++
+    }
+    return zeroCounter
+}
+
+fun getPasswordWithMethod0x434(rotationList: File): Int {
+    var zeroCounter = 0
+    var dial = 50
+
+    rotationList.forEachLine {
+        val magnitude = calculateMagnitude(it)
+        val prevDial = dial
+        dial = rotate(dial, magnitude)
+
+        zeroCounter += abs(magnitude / 100)
+
+        if ((magnitude < 0 && prevDial < dial && prevDial != 0) ||
+            (magnitude > 0 && prevDial > dial && prevDial != 0) ||
+            dial == 0) {
+            zeroCounter++
+        }
+    }
+
+    return zeroCounter
+}
+
+fun main() {
+    val inputFile = File(Path("src/inputs/day1.txt").absolutePathString())
+    val attemptedPassword = getPassword(inputFile)
+    val password = getPasswordWithMethod0x434(inputFile)
+
+    println("Attempted password: $attemptedPassword")
+    println("Password: $password")
+}
---
+++ src/day2.kt
@@ -0,0 +1,145 @@
+import java.io.File
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+import kotlin.math.log10
+import kotlin.math.pow
+
+fun getTopHalf(numStr: String) : Long {
+    return numStr.slice(0..<numStr.length / 2).toLong()
+}
+
+fun getPowerOfTen(exponent: Int) : Long {
+    return (10.0).pow(exponent).toLong()
+}
+
+fun makeRepeated(num: Long) : Long {
+    val numDigits = log10(num.toDouble()).toInt()
+    return num * 10.0.pow(numDigits + 1).toLong() + num
+}
+
+fun getDuplicateSum(min: String, max: String) : Long {
+    var duplicateSum: Long = 0
+
+    var curMin = min
+
+    val minNum = min.toLong()
+    val maxNum = max.toLong()
+
+    while (curMin.length < max.length) {
+        val size = curMin.length
+        if (size % 2 == 0) {
+            for (num in getTopHalf(curMin)..<getPowerOfTen(size / 2)) {
+                val repeatedNum = makeRepeated(num)
+                if (repeatedNum in minNum..maxNum) {
+                    duplicateSum += repeatedNum
+                }
+            }
+        }
+        curMin = "${getPowerOfTen(size)}"
+    }
+
+    if (max.length % 2 == 0) {
+        for (num in getTopHalf(curMin)..getTopHalf(max)) {
+            val repeatedNum = makeRepeated(num)
+            if (repeatedNum in minNum..maxNum) {
+                duplicateSum += repeatedNum
+            }
+        }
+
+    }
+
+    return duplicateSum
+}
+
+fun getAllInvalidIds(idList: File) : Long {
+    var duplicateSum: Long = 0
+
+    val text = idList.readText()
+    val numberRanges = text.split(",")
+
+    for (numberRange in numberRanges) {
+        val numbers = numberRange.split('-')
+        val min = numbers[0].replace("\\s+".toRegex(), "")
+        val max = numbers[1].replace("\\s+".toRegex(), "")
+        duplicateSum += getDuplicateSum(min, max)
+    }
+
+    return duplicateSum
+}
+
+fun getTopDigits(numDigits: Int, numStr: String): String {
+    return numStr.slice(0..<numDigits)
+}
+
+fun getRepeatedNumToSize(num: Long, size: Int): Long {
+    val numStr = "$num"
+    if (size % numStr.length != 0) {
+        throw IllegalArgumentException("Number should be evenly divisible to size.")
+    }
+    return numStr.repeat(size / numStr.length).toLong()
+}
+
+fun getDuplicateSumTwo(min: String, max: String) : Long {
+    var duplicateSum: Long = 0
+
+    var curMin = min
+
+    val minNum = min.toLong()
+    val maxNum = max.toLong()
+
+    val seen = HashSet<Long>()
+
+    while (curMin.length < max.length) {
+        val size = curMin.length
+        for (numDigits in 1..size/2) {
+            if (curMin.length % numDigits != 0) continue
+            val bottomThreshold = getTopDigits(numDigits, curMin).toLong()
+            for (curNum in bottomThreshold..<getPowerOfTen(numDigits)) {
+                val repeated = getRepeatedNumToSize(curNum, curMin.length)
+                if (repeated in minNum..maxNum && repeated !in seen) {
+                    seen.add(repeated)
+                    duplicateSum += repeated
+                }
+            }
+        }
+        curMin = "${getPowerOfTen(size)}"
+    }
+
+
+    for (numDigits in 1..max.length/2) {
+        if (max.length % numDigits != 0) continue
+        val bottomThreshold = getTopDigits(numDigits, curMin).toLong()
+        for (curNum in bottomThreshold..getTopDigits(numDigits, max).toLong()) {
+            val repeated = getRepeatedNumToSize(curNum, max.length)
+            if (repeated in minNum..maxNum && repeated !in seen) {
+                seen.add(repeated)
+                duplicateSum += repeated
+            }
+        }
+    }
+
+    return duplicateSum
+}
+
+fun getAllInvalidIdsTwo(idList: File): Long {
+    var duplicateSum: Long = 0
+
+    val text = idList.readText()
+    val numberRanges = text.split(",")
+
+    for (numberRange in numberRanges) {
+        val numbers = numberRange.split('-')
+        val min = numbers[0].replace("\\s+".toRegex(), "")
+        val max = numbers[1].replace("\\s+".toRegex(), "")
+        duplicateSum += getDuplicateSumTwo(min, max)
+    }
+
+    return duplicateSum
+}
+
+
+fun main() {
+    val inputFile = File(Path("src/inputs/day2.txt").absolutePathString())
+    println(getAllInvalidIds(inputFile))
+    println(getAllInvalidIdsTwo(inputFile))
+}
